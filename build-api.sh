a0b523222a133a248c37e380568189258fade6d9
Tony Lefebvre <tony.lbvre@gmail.com>
2017-01-25 02:50:03 +0100

    Swagger script to use new API classes

--- build-api.sh
+++ build-api.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-( cd main && python lib/endpoints/endpointscfg.py get_swagger_spec user.api.UserCollectionApi --hostname megan-api.endpoints.megan-production.cloud.google.com -o static/ )
+( cd main && python lib/endpoints/endpointscfg.py get_swagger_spec --hostname localhost:8080 -o static/ user.api.UserCollectionApi user.api.UserItemApi)
 

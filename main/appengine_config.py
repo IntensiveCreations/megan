75e01b2eccf0539462b99a8abaac0746853284a4
Tony Lefebvre <tony.lbvre@gmail.com>
2017-02-07 12:46:24 +0000

    Use git submodule for our endpoints-proto-datastore fork

--- main/appengine_config.py
+++ main/appengine_config.py
@@ -1,4 +1,10 @@
 from google.appengine.ext import vendor
+import os
+import sys
+
+ENDPOINTS_PROJECT_DIR = os.path.join(os.path.dirname(__file__),
+                                     'endpoints-proto-datastore')
+sys.path.append(ENDPOINTS_PROJECT_DIR)
 
 # Add any libraries installed in the `lib` folder.
 vendor.add('lib')

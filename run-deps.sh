f624fb96c2d3e3024e3fa12a56bbbf38eea6144a
Tony Lefebvre <tony.lbvre@gmail.com>
2017-02-07 15:04:18 +0000

    move away the endpoints proto datastore from main directory

--- run-deps.sh
+++ run-deps.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 pip install --upgrade -t main/lib -r requirements-vendor.txt
-ln -s $(pwd)/endpoints_proto_datastore/endpoints_proto_datastore main/lib/
+ln -fs $(pwd)/endpoints_proto_datastore/endpoints_proto_datastore main/lib/
